a802030cfb2cb7c5c248b849c5c2497cf64bea00
Karen Baney <Karen_Baney@SHAMROCKFOODS.com>
2021-05-04 11:37:24 -0700

    Fix: add type definition for navigation and add to vue-router in shim.

---
+++ src/router/MetaNavigation.d.ts
@@ -0,0 +1,9 @@
+import { RouteRecordNormalized } from 'vue-router'
+
+export interface MetaNavigation {
+  show: boolean;
+  displayText?: string;
+  icon?: string;
+  route?: RouteRecordNormalized;
+  sectionKey?: number;
+}
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,15 @@
+import { MetaNavigation } from './router/MetaNavigation'
+
 /* eslint-disable */
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    requiresAuth?: boolean,
+    navigation?: MetaNavigation
+  }
 }
